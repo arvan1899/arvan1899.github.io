64321361365890f58297577e94add4a66391529a
Arvan Maulana Iskandar <50607012+arvan1899@users.noreply.github.com>
2019-05-20 16:23:30 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+  /* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
